0669e341a67b4831233494868a50a42b367c68ed
Asad Sabri <asad.sabri19@gmail.com>
2025-11-18 19:47:18 +0500

    remove comment

--- components/mapbox/mapbox.tsx
+++ components/mapbox/mapbox.tsx
@@ -7,8 +7,6 @@ import { useMapContext } from "../hooks/mapContext";
 export default function Mapbox() {
   const { mapContainerRef } = useMapContext();
 
-  // map is already initialized inside the hook (useMapboxFunctions)
-  // so this component only provides the container div
   return (
     <div className="w-full h-full">
       <div ref={mapContainerRef} className="w-full h-full" />
