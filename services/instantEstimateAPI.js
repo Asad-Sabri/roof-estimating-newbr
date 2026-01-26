8919d1bab65c1f4e53221e43b22cdd15c9e905fa
Asad Sabri <asad.sabri19@gmail.com>
2026-01-26 23:45:12 +0500

    remove all comments

--- services/instantEstimateAPI.js
+++ services/instantEstimateAPI.js
@@ -1,11 +1,4 @@
-// services/instantEstimateAPI.js
 import { axiosInstance, handleAPIRequest } from "./axiosInstance";
-
-/**
- * Create Instant Estimate Project
- * This API is called when user completes all steps in instant estimate flow
- * Endpoint: POST /api/instant-estimates
- */
 export const createInstantEstimateAPI = (data) => {
   return handleAPIRequest(
     axiosInstance.post,
@@ -13,12 +6,6 @@ export const createInstantEstimateAPI = (data) => {
     data
   );
 };
-
-/**
- * Update Instant Estimate Project
- * This API is called to update existing estimate (for saving progress on each step)
- * Endpoint: PATCH /api/instant-estimates/{id}
- */ 
 export const updateInstantEstimateAPI = (id, data) => {
   return handleAPIRequest(
     axiosInstance.patch,
